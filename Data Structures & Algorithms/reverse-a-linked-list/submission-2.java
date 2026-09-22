6f5962ab14925c8412fbd34e92c57d992a2dd158
JoshuaRivera209 <123445198+JoshuaRivera209@users.noreply.github.com>
2026-09-22 15:46:51 -0700

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        /*
+            we want to essentially reverse the pointers of each node so they are pointing the other way, making the last non-null node the new head to return.
+
+            we can do this by:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        */
+
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
